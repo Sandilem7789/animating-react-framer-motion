5ee80708c8fbf59d623238e53e93800268d859dc
sandilem7789 <sandilem7789@gmail.com>
2021-04-07 11:29:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,35 +7,41 @@ import purp from "./purp.png";
 import black from "./black.png";
 import green from "./green.png";
 
+import { motion } from "framer-motion";
+
 function App() {
   return (
-    <div>
-      <Header>
-        <Menu />
-        <h1>Header</h1>
-      </Header>
-      <Container>
-        <h2>Super Cool</h2>
-        <CardGrid>
-          <Card style={{ background: "var(--purp)" }}>
-            <h3>Some card</h3>
-            <img src={purp} />
-          </Card>
-          <Card style={{ background: "var(--blue)" }}>
-            <h3>Some card</h3>
-            <img src={blue} />
-          </Card>
-          <Card style={{ background: "var(--black)" }}>
-            <h3>Some card</h3>
-            <img src={black} />
-          </Card>
-          <Card style={{ background: "var(--green)" }}>
-            <h3>Some card</h3>
-            <img src={green} />
-          </Card>
-        </CardGrid>
-      </Container>
-    </div>
+    <motion.div
+		initial={{opacity: 0, x: 0, y: -300, skewX: 70}}
+		animate={{opacity: 1, x: 0, y: 0, skewX: 0}}
+		transition={{duration: 0.5}}
+	>
+		<Header>
+			<Menu />
+			<h1>Header</h1>
+		</Header>
+		<Container>
+			<h2>Super Cool</h2>
+			<CardGrid>
+			<Card style={{ background: "var(--purp)" }}>
+				<h3>Some card</h3>
+				<img alt="" src={purp} />
+			</Card>
+			<Card style={{ background: "var(--blue)" }}>
+				<h3>Some card</h3>
+				<img alt="" src={blue} />
+			</Card>
+			<Card style={{ background: "var(--black)" }}>
+				<h3>Some card</h3>
+				<img alt="" src={black} />
+			</Card>
+			<Card style={{ background: "var(--green)" }}>
+				<h3>Some card</h3>
+				<img alt="" src={green} />
+			</Card>
+			</CardGrid>
+		</Container>
+    </motion.div>
   );
 }
 
